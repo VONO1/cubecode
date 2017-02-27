d4a5928bc1ab38eceef89eb5f633b7a8fd0dea5f
VONO1 <Rb9427377>
2017-02-27 12:58:49 +0300

    2222

--- [2]27.02.2017.py
+++ [2]27.02.2017.py
@@ -1,14 +1,19 @@
+
+def PAL(zn):
+    a = list(zn)
+    aa = a
+
+
+    bb = []
+    for a in reversed(a):
+        bb.append(a)
+
+
+    print(aa, bb)
+    if aa == bb:
+        print("По всей видимости это Палиндром")
+    else: print("По всей видимости это НЕ Палиндром")
+
+
 pl = input('Введите значение: ')
-a = list(pl)
-aa = a
-
-
-bb = []
-for a in reversed(a):
-    bb.append(a)
-
-
-print(aa, bb)
-if aa == bb:
-    print("По всей видимости это Палиндром")
-else: print("По всей видимости это НЕ Палиндром")
+PAL(pl)
