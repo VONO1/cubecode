a55073842a58151b7ac8241e12fd41e1eb4f5b51
VONO1 <Rb9427377>
2017-03-03 18:04:04 +0300

    2222

--- SQLite/SQL.py
+++ SQLite/SQL.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sqlite3
 #Подключение к базе
-conn = sqlite3.connect('my.sqlite2')
+conn = sqlite3.connect('my.sqlite')
 #Создание курсора
 c = conn.cursor()
 
@@ -9,9 +9,26 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS stocks
              (name text, txt text, time text, status text)''')
 
-# Insert a row of data
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100)")
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100)")
+
+
+SQL_SELECT = '''
+    SELECT
+        id, short_url, original_url, created
+    FROM
+        shortener
+'''
+purchases = [('6662006-01-05','666BUY','RHAT',100),
+             ('6662006-04-05', '666BUY', 'MSFT', 100),
+             ('6662006-04-06', '666SELL', 'IBM', 500),
+            ]
+c.executemany('INSERT INTO stocks VALUES (?,?,?,?)', purchases)
+
+
+
+for row in c.execute('SELECT * FROM stocks ORDER BY txt'):
+        print(row)
+
+
 
 # Save (commit) the changes
 conn.commit()
@@ -19,5 +36,5 @@ conn.commit()
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
 conn.close()
-print(c)
+
 info = ["id", "name", "txt", "time", "status"]
